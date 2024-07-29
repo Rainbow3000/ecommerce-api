9596a90a9e067cac4f2a7e074fa20c256d46a38a
thinhnd <nguyenducthinh0401@gmail.com>
2024-07-29 10:50:37 +0700

    fix: import comment module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { ProductModule } from './modules/product/product.module';
 import { OrderModule } from './modules/order/order.module';
 import { OrderDetailsModule } from './modules/order-details/order-details.module';
 import { ChatModule } from './modules/chat/chat.module';
+import { CommentModule } from './modules/comment/comment.module';
 
 @Module({
   imports: [
@@ -41,6 +42,7 @@ import { ChatModule } from './modules/chat/chat.module';
     ProductModule,
     OrderModule,
     OrderDetailsModule,
+    CommentModule,
     ChatModule,
     JwtModule,
   ],
